5f4d9ee6acb2c880406b9398178e82f36c2adbd9
invokerqqe <119207896+invokerqqe@users.noreply.github.com>
2022-11-27 21:39:51 +0600

    Add files via upload

---
+++ #1/8/Start.cmd
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+python scr1.py | ConsoleApp.exe
+
+PAUSE
---
+++ #1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp;
+
+class Program
+{
+    static void main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        Console.WriteLine("This is CODE CSharpp");
+        Console.WriteLine(s);
+        Console.WriteLine("This is code CSharp");
+    }
+}
+
